ec49c2930f163d40a0e258f9085162dd8ac48f75
Johannes <johannes.przymusinski@rz.uni-regensburg.de>
2019-10-23 12:26:33 +0200

    merged js / css files, fixed links in base template and removed old files

--- templates/base.html.php
+++ templates/base.html.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>00:00:00</title>
-    <meta charset="utf-8" />
-    <meta
-      name="description"
-      content="See the current time right in you're favourite tool - the browser"
-    />
-    <link rel="stylesheet" href="./src/main.css" />
-    <link rel="shortcut icon" type="image/x-icon" href="https://raw.githubusercontent.com/browser-clock/assets/master/logo/v1.0.0/ico/48x48.ico">
-  </head>
-  <body>
-    <include href="utils/github-corner.html.php" />
-    <include href="widgets/clock-face.html.php" />
-    <span class="copy">&copy; <a href="mailto:johannes@przymusinski.de">Johannes Stephan Przymusinsk</a>, 2019</span>
-  </body>
-  <script src="./src/main.js" ></script>
+
+<head>
+  <title>00:00:00</title>
+  <meta charset="utf-8" />
+  <meta name="description" content="See the current time right in you're favourite tool - the browser" />
+  <link rel="stylesheet" href="templates/res/main.css" />
+  <link rel="shortcut icon" type="image/x-icon" href="https://raw.githubusercontent.com/browser-clock/assets/master/logo/v1.0.0/ico/48x48.ico">
+</head>
+
+<body>
+  <include href="utils/github-corner.html.php" />
+  <include href="widgets/clock-face.html.php" />
+  <span class="copy">&copy; <a href="mailto:johannes@przymusinski.de">Johannes Stephan Przymusinsk</a>, 2019</span>
+</body>
+<script src="templates/res/main.js"></script>
+
 </html>
