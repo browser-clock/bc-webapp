6c862f3b5569c07d3b16b69c1df8d84c7afc1c81
Johannes <johannes.przymusinski@rz.uni-regensburg.de>
2019-10-23 12:22:10 +0200

    merged old html code to templates

---
+++ tmp/20aabpl4t9j48.1k2il5wrpx2ai.php
@@ -0,0 +1,21 @@
+<div class="clock-face">
+    <div class="left">
+        <div class="short">
+            <span class="char" data-index="0">A</span>
+            <span class="char" data-index="1">M</span>
+        </div>
+        <div class="colon">
+            <span class="char" data-index="0">:</span>
+        </div>
+    </div>
+    <div class="time">
+        <div class="hour">
+            <span class="digit" data-index="0">0</span>
+            <span class="digit" data-index="1">0</span>
+        </div>
+        <div class="minute">
+            <span class="digit" data-index="0">0</span>
+            <span class="digit" data-index="1">0</span>
+        </div>
+    </div>
+</div>
