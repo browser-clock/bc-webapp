de19cb222eae54588c607638961c892f2febec43
Johannes Przymusinski <johannes@przymusinski.de>
2019-11-05 11:15:00 +0100

    create config for migrations

---
+++ migrations.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "name" => "bc-webapp",
+    "migrations_namespace" => "App\Migrations",
+    "table_name" => "doctrine_migrations_versions",
+    "column_name" => "version",
+    "migrations_directory" => "app/Migrations"
+];
