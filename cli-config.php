30481776ad181f4a70ed4052614b841775f77ae6
Johannes Przymusinski <johannes@przymusinski.de>
2019-11-05 11:12:07 +0100

    add missing comments

--- cli-config.php
+++ cli-config.php
@@ -6,11 +6,14 @@ use Symfony\Component\Console\Helper\HelperSet;
 
 require_once "vendor/autoload.php";
 
+// Initiate FatFree
 $f3 = Base::instance();
 $f3->config("config.ini");
 
+// load the DB variables from config.ini
 $db = $f3->get("DB");
 
+// create connection
 $connection = DriverManager::getConnection([
     "host" => $db["HOSTNAME"],
     "user" => $db["USERNAME"],
@@ -19,6 +22,7 @@ $connection = DriverManager::getConnection([
     "driver" => $db["DRIVER"],
 ]);
 
+// return connection
 return new HelperSet([
     "db" => new ConnectionHelper($connection),
 ]);
