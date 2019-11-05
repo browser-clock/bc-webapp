0a8003eb46a7985149227810dde25af0898b9999
Johannes Przymusinski <johannes@przymusinski.de>
2019-11-05 12:16:21 +0100

    load config.ini

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require_once "vendor/autoload.php";
 
 $f3 = Base::instance();
 
+// load config files
 $f3->config("app/config/config.cfg");
+
+// config.ini after all other config files to prevent overriding user variables
+$f3->config("config.ini");
 
 $f3->run();
